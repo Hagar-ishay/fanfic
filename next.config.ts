25b97da04606e29fee4ee40c1d53e0bb07d3578f
Hagar-ishay <hagari0297@gmail.com>
2024-11-24 18:57:22 +0200

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["epub", "epub-gen"],
 };
 
 export default nextConfig;
