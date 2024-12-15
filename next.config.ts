254cc3ff678f9e506b4c58bba8817875115153f8
Hagar-ishay <hagari0297@gmail.com>
2024-12-15 07:14:00 +0200

    remove wepback stuff

--- next.config.ts
+++ next.config.ts
@@ -6,17 +6,9 @@ const nextConfig: NextConfig = {
     "epub-gen",
     "tough-cookie",
     "axios-cookiejar-support",
-  ], // Explicitly include tough-cookie and axios-cookiejar-support
+  ],
   experimental: {
     dynamicIO: true,
-  },
-  webpack: (config) => {
-    config.externals = {
-      ...config.externals,
-      "tough-cookie": "commonjs tough-cookie",
-      "axios-cookiejar-support": "commonjs axios-cookiejar-support",
-    };
-    return config;
   },
 };
 
