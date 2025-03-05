273ec9949e901349456a4c32ee29242d61f80949
Hagar-ishay <hagari0297@gmail.com>
2025-03-05 07:50:54 +0200

    consts

--- app/consts.ts
+++ app/consts.ts
@@ -1,9 +1,6 @@
 export const AO3_LINK = "https://archiveofourown.gay";
 
-export const VALID_AO3_URLS = [
-  "https://archiveofourown.org/works/",
-  "https://archiveofourown.gay/works/",
-];
+export const VALID_AO3_URLS = ["https://archiveofourown.org", "https://archiveofourown.gay"];
 
 export const TAGS = {
   RATING: "RATING",
